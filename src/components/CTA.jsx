27aaf51811f157023007ccf93751b684b5bc903e
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-09 18:06:02 +0100

    added CTA (bottom) component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,15 @@
-export function CTA() {
+import {Button} from "./Button";
+
+export function CTA({company, position, apply}) {
     return (
-        <div>CTA</div>
+        <div className="absolute p-10 w-full xs:static bg-white mt-10">
+            <div className="flex justify-center sm:justify-between items-center">
+                <div className="hidden sm:inline-block">
+                    <h2>{position}</h2>
+                    <p>{company}</p>
+                </div>
+                <Button link={apply} text={'Apply Now'} primary/>
+            </div>
+        </div>
     )
 }
