ba35ced164672b5c6494cf741e85c633b8f87455
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-09 20:30:13 +0100

    data props deconstructed (code refactor)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,23 +2,33 @@ import {Link} from "react-router-dom";
 
 export function Card({data}) {
 
+    const {
+        logoBackground,
+        logo,
+        company,
+        postedAt,
+        contract,
+        position,
+        location
+    } = data;
+
     return (
             <div className="p-6 bg-white rounded relative">
                 <div className="absolute top-0 h-14 w-14 rounded-xl -translate-y-1/2"
-                     style={{backgroundColor: data.logoBackground}}>
-                    <img src={process.env.PUBLIC_URL + data.logo} alt={data.company + ' logo'}
+                     style={{backgroundColor: logoBackground}}>
+                    <img src={process.env.PUBLIC_URL + logo} alt={company + ' logo'}
                          className="p-2 h-full w-full object-center object-contain"/>
                 </div>
                 <p className="mt-4">
-                    <span className="mr-2">{data.postedAt}</span>
+                    <span className="mr-2">{postedAt}</span>
                     <span className="mr-2">.</span>
-                    <span>{data.contract}</span>
+                    <span>{contract}</span>
                 </p>
-                <Link to={`/jobs/${data.company}-${data.position}`} state={data}>
-                    <h2 className="mt-1 font-bold">{data.position}</h2>
+                <Link to={`/jobs/${company}-${position}`} state={data}>
+                    <h2 className="mt-1 font-bold">{position}</h2>
                 </Link>
-                <h3 className="mt-1">{data.company}</h3>
-                <p className="mt-8">{data.location}</p>
+                <h3 className="mt-1">{company}</h3>
+                <p className="mt-8">{location}</p>
             </div>
     )
 }
