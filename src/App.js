cebd8692070ba18f3858bc4cfc791b285ea9c6be
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-14 09:45:26 +0100

    moved dark mode class set to Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {jobPositions} from './data'
 import {useEffect, useState} from "react";
+import {jobPositions} from './data'
 import {Header} from "./components/Header";
 import {Home} from "./pages/Home";
 import {Job} from "./pages/Job";
@@ -17,17 +17,6 @@ function App() {
                 ...item,
                 mix: item.position + item.company
             })));
-    }, [])
-
-    useEffect(() => {
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-        }
-        localStorage.theme = 'light'
-        localStorage.theme = 'dark'
-        localStorage.removeItem('theme')
     }, [])
 
     return (
